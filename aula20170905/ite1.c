77894bdb832b1a2c4e68487f62dfafcf5db55e4d
guilhermeaam <31656068+guilhermeaam@users.noreply.github.com>
2017-09-11 11:48:37 -0300

    Update ite1.c

--- aula20170905/ite1.c
+++ aula20170905/ite1.c
@@ -4,8 +4,8 @@ int main ()
 	int n, f=1, i;
 	printf("Digite um numero para se achar o fatorial\n");
     scanf("%d",&n);
-	for (i=1;i<=n;i++);
-	f=f*i;
+	for (i=0;i<n;i++)
+	f*=(n-i);
     printf("Resultado ");
     printf("%d\n", f);
     return 0;

96f5c3300109c5c4a60ecd1e98333c94f5a029d9
guilhermeaam <guilhermeafosno80@gmail.com>
2017-10-05 10:26:39 -0300

    fim de aula

--- aula20171005/ptr6.c
+++ aula20171005/ptr6.c
@@ -7,10 +7,11 @@ int main(){
         str[i] = '\0';
     printf("Entre com texto (%d carac):\n", N-1);
     fgets(str, N, stdin);
-    printf("\nCodigo:\n");
+    printf("\nCodigo:");
     for(i = 0; i < N/sizeof(int); i++) printf("%d ", varredura[i]);
+    printf("\nHex:");
 	for(i = 0; i < N/sizeof(int); i++)
-       printf("\n%X (hex)",varredura[i]);
+       printf("%X ",varredura[i]);
     printf("\n");
     return 0;
 }

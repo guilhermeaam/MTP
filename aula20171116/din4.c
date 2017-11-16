32c0d7b0c6093d514d8b75426733d4b68386e72e
guilhermeaam <guilhermeafonso80@gmail.com>
2017-11-16 10:57:51 -0200

    fim de aula

--- aula20171116/din4.c
+++ aula20171116/din4.c
@@ -1,4 +1,24 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <math.h>
-
+int main(){
+	int v, p,i=0,j=0, x,y,a;
+	printf("Entre com o numero de poligonos: ");
+	scanf("%d", &p);
+	while (i<p){
+	printf("Entre com o numero de vertices do poligono %d: ",i+1);
+	scanf("%d", &v);
+	while(j<v){
+	printf("Entre com o x do %d vertice: ",j+1);
+	scanf("%d", &x);
+	printf("Entre com o y do %d vertice: ",j+1);
+	scanf("%d", &y);
+	j++;
+	printf("A area do poligono %d: ",i+1);
+	printf("%d: ",a);
+	}
+	j=0;
+	i++;
+	}
+	return 0;
+	}
